ff0f01f0911c544cd93c917b69033080fd8227ec
DarsanaBoraian <129121551+DarsanaBoraian@users.noreply.github.com>
2023-09-06 16:39:13 +0100

    Tests updated

--- app/src/test/java/com/example/tflapp/ui/DashboardViewModelTest.kt
+++ app/src/test/java/com/example/tflapp/ui/DashboardViewModelTest.kt
@@ -1,8 +1,26 @@
 package com.example.tflapp.ui
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
 
 class DashboardViewModelTest {
 
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
 
+    @Test
+    fun getTFLDataTest() {
+        val mockRepository = com.example.tflapp.repository.MockRepositoryImpl()
+        val viewModel = DashboardViewModel(mockRepository)
+        Assert.assertEquals(0, viewModel.tflData.value.size)
+        runTest {
+            advanceUntilIdle()
+        }
+        Assert.assertEquals(1, viewModel.tflData.value.size)
+    }
 }
